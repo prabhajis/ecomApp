2f6384250a453129f9d218282ca99d83ae36c2ce
prabhajis <40330384+prabhajis@users.noreply.github.com>
2024-02-08 17:15:07 +0530

    Update EcomContoller.java

--- src/main/java/com/demo/ecomApp/controller/EcomContoller.java
+++ src/main/java/com/demo/ecomApp/controller/EcomContoller.java
@@ -11,15 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Slf4j
-@Controller
+@RestController
 @RequestMapping(value = "/ecom-service/v1")
 public class EcomContoller {
     @Autowired
